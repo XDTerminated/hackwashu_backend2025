dc8fc81a58d5161967ad3ea67b06a81d49a033b2
XDTerminated <gupta.sayam2006@gmail.com>
2025-10-24 21:31:58 -0500

    uvicorn setup

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import fastapi
+from fastapi import FastAPI
 from dotenv import load_dotenv
 import psycopg
 import os
+
+app = FastAPI()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
